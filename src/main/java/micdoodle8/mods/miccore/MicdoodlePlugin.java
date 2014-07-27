f47a0183805446ec88e5e7cc3bed79f5ec6ae8a2
Kyle Herock <kherock@gmail.com>
2014-07-27 00:48:05 -0400

    Update to 1.7.10

--- src/main/java/micdoodle8/mods/miccore/MicdoodlePlugin.java
+++ src/main/java/micdoodle8/mods/miccore/MicdoodlePlugin.java
@@ -29,7 +29,7 @@ import java.util.Map;
 public class MicdoodlePlugin implements IFMLLoadingPlugin, IFMLCallHook
 {
 	public static boolean hasRegistered = false;
-	public static final String mcVersion = "[1.7.2]";
+	public static final String mcVersion = "[1.7.10]";
 	public static File mcDir;
     public static File canonicalConfigDir;
 
