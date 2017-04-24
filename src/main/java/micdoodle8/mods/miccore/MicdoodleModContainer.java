ef84116694f1538d884874fafc419879c39e8003
Galacticraft developer <radfast@users.noreply.github.com>
2017-04-24 11:54:31 +0100

    Add MCVersion annotation to Coremod

--- src/main/java/micdoodle8/mods/miccore/MicdoodleModContainer.java
+++ src/main/java/micdoodle8/mods/miccore/MicdoodleModContainer.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+@MCVersion("1.8.9")
 public class MicdoodleModContainer extends DummyModContainer
 {
 	public MicdoodleModContainer()
